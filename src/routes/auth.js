b4fff5f854398d3292bbdd05eaad24ab2ad8b1c6
Rodrigo Mota <rodrigomota@outlook.com>
2020-04-28 14:48:04 -0300

    implementa confirmação de email

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,10 +3,16 @@ const router = express.Router()
 
 const controller = require('../controllers/auth')
 
+const authMiddleare = require('../middlewares/auth')
+
 router.post('/register', controller.register)
 router.post('/login', controller.login)
 router.post('/forgot_password', controller.forgotPassword)
 router.post('/reset_password', controller.resetPassword)
 router.post('/validateToken', controller.validateToken)
+router.get('/confirmEmail/:token', controller.confirmEmail)
+
+router.use(authMiddleare)
+router.get('/resendEmailConfirmation', controller.resendEmailConfirmation)
 
 module.exports = router
